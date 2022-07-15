dbbc56b6443a16e6bdcb73b2466720303871c045
Greidy Peña <greidykp@gmail.com>
2022-07-15 18:22:45 -0500

    Creacion de componente para cada paciente

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,21 +1,19 @@
+import Paciente from "./Paciente";
 
 function ListadoPacientes() {
   return (
-    <div className="md:w-1/2 lg:3/5">
+    <div className="md:w-1/2 lg:3/5 md:h-screen overflow-y-scroll">
         <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
         <p className="text-lg mt-2 mb-8 text-center">
              Administra tus {''}
             <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
         </p>
-        <div>
-            <p>
-                <span>
-                    
-                </span>
-            </p>
-        </div>
+        <Paciente />
+        <Paciente />
+        <Paciente />
+        <Paciente />
     </div>
   )
 }
 
-export default ListadoPacientes
+export default ListadoPacientes;
